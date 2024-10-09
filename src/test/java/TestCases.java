1f9c4b4370e821e0894e3285133e78cfd2573284
dpruthi <dpruthi16@gmail.com>
2024-10-09 11:42:13 +0530

    checking sonar

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -5,16 +5,19 @@ public class TestCases {
 
     @Test
     public void verifyAddition() {
+        System.out.println("Hello world!");
         Assert.assertEquals(2 + 2, 4);
     }
 
     @Test
     public void verifySubtraction() {
+        System.out.println("");
         Assert.assertEquals(4 - 2, 2);
     }
 
     @Test
     public void verifyMultiply() {
+        System.out.println("wfwefewijfjiwejifweifbhiwfhibfrhibfbrihjfwijrfhiwhfijwehrfhwehifihwjefijewhfhrwihfiwehgfiehwjbgfihrihuiwi");
         Assert.assertEquals(4 * 2, 8);
     }
 }
