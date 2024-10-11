dff3ef9d712c110536d90504de31318646111b37
dpruthi <dpruthi16@gmail.com>
2024-10-11 13:31:23 +0530

    sonar fail check

--- src/main/java/org/nagp/Main.java
+++ src/main/java/org/nagp/Main.java
@@ -3,6 +3,11 @@ package org.nagp;
 public class Main {
 
     public static void main(String[] args) {
+        if(true){
+            System.out.println("");
+        } else if (false) {
+            System.out.println("");
+        }
         System.out.println("Hello worldjhbijhjihhhubjbgbvvvvvvbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb!");
     }
 }
