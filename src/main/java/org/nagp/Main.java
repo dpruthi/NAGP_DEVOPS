ea93c09d3c75e751d0e32ad26f83a1fc914dc8f1
dpruthi <dpruthi16@gmail.com>
2024-10-12 16:47:55 +0530

    sonar break

--- src/main/java/org/nagp/Main.java
+++ src/main/java/org/nagp/Main.java
@@ -3,6 +3,11 @@ package org.nagp;
 public class Main {
 
     public static void main(String[] args) {
+        if (true) {
+            System.out.println("hello");
+        }else if (false){
+            System.out.println("hello");
+        }
         System.out.println("hello");
         add(2, 2);
     }
