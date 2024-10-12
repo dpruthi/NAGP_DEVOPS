b9e32e594bbc53d258f996b9e6c44e001950fbe4
dpruthi <dpruthi16@gmail.com>
2024-10-12 13:41:12 +0530

    remove code break syntax

--- src/main/java/org/nagp/Main.java
+++ src/main/java/org/nagp/Main.java
@@ -2,4 +2,11 @@ package org.nagp;
 
 public class Main {
 
+    public static void main(String[] args) {
+        add(2, 2);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
